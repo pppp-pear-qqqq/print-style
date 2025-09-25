4d4b9159d0d248ffa0330df5f2f238632e63651a
itt-n <itt_nakashima@worksid.co.jp>
2025-09-25 18:03:51 +0900

    インデント調整

--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -4,43 +4,40 @@ use syn::{DeriveInput, parse_macro_input};
 
 #[proc_macro_derive(AnsiStylize)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input = &parse_macro_input!(input as DeriveInput);
+	let input = &parse_macro_input!(input as DeriveInput);
 
-    match generate(input) {
-        Ok(generated) => generated,
-        Err(err) => err.to_compile_error().into(),
-    }
+	match generate(input) {
+		Ok(generated) => generated,
+		Err(err) => err.to_compile_error().into(),
+	}
 }
 
 fn generate(input: &DeriveInput) -> Result<TokenStream, syn::Error> {
-    // enum情報を取得
-    let data = match &input.data {
-        syn::Data::Enum(v) => v,
-        _ => {
-            return Err(syn::Error::new_spanned(&input.ident, "enum型が必要です"));
-        }
-    };
-    // 各値を取得してTokenStreamに変換
-    let mut variants = Vec::new();
-    for v in &data.variants {
-        if !v.fields.is_empty() {
-            return Err(syn::Error::new_spanned(
-                &v.fields,
-                "フィールドは存在できません",
-            ));
-        }
-        let ident = &v.ident;
-        variants.push(quote! {
-            fn #ident(&self) -> Styled<'_, Self> {
-                self.with(vec![Effect::Style(Ansi::#ident)])
-            }
-        });
-    }
-    let expanded = quote! {
-        pub trait AnsiStylize: Stylize {
-            #(#variants)*
-        }
-        impl<T: Stylize> AnsiStylize for T {}
-    };
-    Ok(expanded.into())
+	// enum情報を取得
+	let data = match &input.data {
+		syn::Data::Enum(v) => v,
+		_ => {
+			return Err(syn::Error::new_spanned(&input.ident, "enum型が必要です"));
+		}
+	};
+	// 各値を取得してTokenStreamに変換
+	let mut variants = Vec::new();
+	for v in &data.variants {
+		if !v.fields.is_empty() {
+			return Err(syn::Error::new_spanned(&v.fields, "フィールドは存在できません"));
+		}
+		let ident = &v.ident;
+		variants.push(quote! {
+			fn #ident(&self) -> Styled<'_, Self> {
+				self.with(vec![Effect::Style(Ansi::#ident)])
+			}
+		});
+	}
+	let expanded = quote! {
+		pub trait AnsiStylize: Stylize {
+			#(#variants)*
+		}
+		impl<T: Stylize> AnsiStylize for T {}
+	};
+	Ok(expanded.into())
 }
